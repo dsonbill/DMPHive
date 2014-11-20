702200e052fcc9905a1d7a86aaa4e4a8e13a2047
William Donaldson <dson.bill@gmail.com>
2014-11-20 04:00:10 -0700

    Cleaned Up xmlrsa and Added client.conf and PrivKey.xml to .gitignore

--- xmlrsa.py
+++ xmlrsa.py
@@ -38,16 +38,16 @@ class RSA():
         p_xml = rsa_xml.find('P')
         q_xml = rsa_xml.find('Q')
 
-        modulus_int = bytes_to_int(base64.b64decode(modulus_xml.text))
-        modulus_bytes = base64.b64decode(modulus_xml.text)
+        modulus_int = bytes_to_int(base64.standard_b64decode(modulus_xml.text))
+        modulus_bytes = base64.standard_b64decode(modulus_xml.text)
         modulus_bytes_tested = bytes_from_int(bytes_to_int(modulus_bytes))
         if modulus_bytes == modulus_bytes_tested:
             print("Everything is good")
 
-        exponent_int = bytes_to_int(base64.b64decode(exponent_xml.text))
-        d_int = bytes_to_int(base64.b64decode(d_xml.text))
-        p_int = bytes_to_int(base64.b64decode(p_xml.text))
-        q_int = bytes_to_int(base64.b64decode(q_xml.text))
+        exponent_int = bytes_to_int(base64.standard_b64decode(exponent_xml.text))
+        d_int = bytes_to_int(base64.standard_b64decode(d_xml.text))
+        p_int = bytes_to_int(base64.standard_b64decode(p_xml.text))
+        q_int = bytes_to_int(base64.standard_b64decode(q_xml.text))
 
         # Set key objects
         self.public_key = rsa.PublicKey(modulus_int, exponent_int)
@@ -61,46 +61,35 @@ class RSA():
 
     def save_keys_xml(self, filename_private_key):
         if self.initialized:
-            rsa_key_value_xml = ET.Element('RSAKeyValue')
-            modulus_xml = ET.SubElement(rsa_key_value_xml, 'Modulus')
-            exponent_xml = ET.SubElement(rsa_key_value_xml, 'Exponent')
-            d_xml = ET.SubElement(rsa_key_value_xml, 'D')
-            p_xml = ET.SubElement(rsa_key_value_xml, 'P')
-            q_xml = ET.SubElement(rsa_key_value_xml, 'Q')
-
-            modulus_xml.text = base64.b64encode(bytes_from_int(self.private_key.n)).decode("utf-8")
-            exponent_xml.text = base64.b64encode(bytes_from_int(self.private_key.e)).decode("utf-8")
-            d_xml.text = base64.b64encode(bytes_from_int(self.private_key.d)).decode("utf-8")
-            p_xml.text = base64.b64encode(bytes_from_int(self.private_key.p)).decode("utf-8")
-            q_xml.text = base64.b64encode(bytes_from_int(self.private_key.q)).decode("utf-8")
-
-            xml_string = ET.tostring(rsa_key_value_xml).decode()
-            print(xml_string)
             with open(filename_private_key, 'w') as file:
-                file.write(xml_string)
+                file.write(self.private_key_xml)
 
     @staticmethod
     def get_keys_xml_string(private_key):
-        rsa_key_value_xml_pub = ET.Element('RSAKeyValue')
-        modulus_xml_pub = ET.SubElement(rsa_key_value_xml_pub, 'Modulus')
-        exponent_xml_pub = ET.SubElement(rsa_key_value_xml_pub, 'Exponent')
+        rsa_key_value_xml = ET.Element('RSAKeyValue')
+        modulus_xml = ET.SubElement(rsa_key_value_xml, 'Modulus')
+        exponent_xml = ET.SubElement(rsa_key_value_xml, 'Exponent')
 
-        modulus_xml_pub.text = base64.b64encode(bytes_from_int(private_key.n)).decode("utf-8")
-        exponent_xml_pub.text = base64.b64encode(bytes_from_int(private_key.e)).decode("utf-8")
+        modulus_xml.text = base64.standard_b64encode(bytes_from_int(private_key.n)).decode("utf-8")
+        exponent_xml.text = base64.standard_b64encode(bytes_from_int(private_key.e)).decode("utf-8")
 
-        rsa_key_value_xml_private = ET.Element('RSAKeyValue')
-        modulus_xml_private = ET.SubElement(rsa_key_value_xml_private, 'Modulus')
-        exponent_xml_private = ET.SubElement(rsa_key_value_xml_private, 'Exponent')
-        d_xml_private = ET.SubElement(rsa_key_value_xml_private, 'D')
-        p_xml_private = ET.SubElement(rsa_key_value_xml_private, 'P')
-        q_xml_private = ET.SubElement(rsa_key_value_xml_private, 'Q')
+        pubkey = ET.tostring(rsa_key_value_xml).decode('utf-8')
 
-        modulus_xml_private.text = base64.b64encode(bytes_from_int(private_key.n)).decode("utf-8")
-        exponent_xml_private.text = base64.b64encode(bytes_from_int(private_key.e)).decode("utf-8")
-        d_xml_private.text = base64.b64encode(bytes_from_int(private_key.d)).decode("utf-8")
-        p_xml_private.text = base64.b64encode(bytes_from_int(private_key.p)).decode("utf-8")
-        q_xml_private.text = base64.b64encode(bytes_from_int(private_key.q)).decode("utf-8")
+        d_xml = ET.SubElement(rsa_key_value_xml, 'D')
+        p_xml = ET.SubElement(rsa_key_value_xml, 'P')
+        q_xml = ET.SubElement(rsa_key_value_xml, 'Q')
+        dp_xml = ET.SubElement(rsa_key_value_xml, 'DP')
+        dq_xml = ET.SubElement(rsa_key_value_xml, 'DQ')
+        inverseq_xml = ET.SubElement(rsa_key_value_xml, 'InverseQ')
 
-        return ET.tostring(rsa_key_value_xml_pub).decode('utf-8'),\
-            ET.tostring(rsa_key_value_xml_private).decode('utf-8')
+        d_xml.text = base64.standard_b64encode(bytes_from_int(private_key.d)).decode("utf-8")
+        p_xml.text = base64.standard_b64encode(bytes_from_int(private_key.p)).decode("utf-8")
+        q_xml.text = base64.standard_b64encode(bytes_from_int(private_key.q)).decode("utf-8")
+        dp_xml.text = base64.standard_b64encode(bytes_from_int(private_key.exp1)).decode('utf-8')
+        dq_xml.text = base64.standard_b64encode(bytes_from_int(private_key.exp2)).decode('utf-8')
+        inverseq_xml.text = base64.standard_b64encode(bytes_from_int(private_key.coef)).decode('utf-8')
+
+        privkey = ET.tostring(rsa_key_value_xml).decode('utf-8')
+
+        return pubkey, privkey
 
